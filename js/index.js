6a05f2c8fd9408fd53790dbdc7b0f6c627bc50bf
Aires <aires@localhost.localdomain>
2019-10-09 12:48:26 -0300

    First commit - Basic structure after Programming Kata

---
+++ js/index.js
@@ -0,0 +1,60 @@
+var cookieApp = angular.module("cookie", []);
+
+cookieApp.factory("FactCookieJar", function() {
+    return { qty: 0 };
+});
+
+cookieApp.factory("FactOvens", function() {
+    return { 
+        qty: 1,
+        cost: 10
+    };
+});
+
+cookieApp.factory("FactGrandmas", function() {
+    return {
+        qty: 0,
+        cost: 100
+    };
+});
+
+cookieApp.controller("cookieJar", function($scope, FactCookieJar, FactOvens) {
+    $scope.cookies = FactCookieJar;
+    $scope.ovens = FactOvens;
+
+    $scope.addCookie = function() {
+        $scope.cookies.qty += $scope.ovens.qty;
+    }
+});
+
+cookieApp.controller("ovens", function($scope, FactCookieJar, FactOvens) {
+    $scope.ovens = FactOvens;
+
+    $scope.buyOven = function() {
+        if(FactCookieJar.qty >= $scope.ovens.cost)
+        {
+            FactCookieJar.qty -= $scope.ovens.cost;
+            $scope.ovens.qty += 1;
+            $scope.ovens.cost += 10;
+        }
+    };
+});
+
+
+cookieApp.controller("grandmas", function($scope, $interval, FactCookieJar, FactGrandmas) {
+    $scope.grandmas = FactGrandmas;
+
+    $scope.buyGrandma = function() {
+        if(FactCookieJar.qty >= $scope.grandmas.cost)
+        {
+            FactCookieJar.qty -= $scope.grandmas.cost;
+            $scope.grandmas.qty += 1;
+            $scope.grandmas.cost += 100;
+        }
+    };
+
+    var update = $interval(function() {
+        FactCookieJar.qty += FactGrandmas.qty;
+    }, 1000); //every second
+});
+
