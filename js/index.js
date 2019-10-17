007af397f148de81ba37cc16a1aa74f5a0031ced
Friar Hob <github@friarhob.33mail.com>
2019-10-16 22:08:29 -0300

    Adding Retirement Homes (which add Grandmas through time)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var cookieApp = angular.module("cookie", []);
 
 cookieApp.factory("FactCookieJar", function() {
-    return { qty: 0 };
+    return { qty: 1000000 };
 });
 
 cookieApp.factory("FactOvens", function() {
@@ -14,14 +14,24 @@ cookieApp.factory("FactOvens", function() {
 cookieApp.factory("FactGrandmas", function() {
     return {
         qty: 0,
-        cost: 100
+        cost: 100,
+        update: null
     };
 });
 
 cookieApp.factory("FactFactories", function() {
     return {
         qty: 0,
-        cost: 1000
+        cost: 1000,
+        update: null
+    };
+});
+
+cookieApp.factory("FactRetirementHomes", function() {
+    return {
+        qty: 0,
+        cost: 10000,
+        update: null
     };
 });
 
@@ -62,7 +72,9 @@ cookieApp.controller("grandmas", function($scope, $interval, FactCookieJar, Fact
         }
     };
 
-    var update = $interval(function() {
+    if($scope.grandmas.update)
+        $interval.cancel($scope.grandmas.update);
+    $scope.grandmas.update = $interval(function() {
         $scope.cookies.qty += $scope.grandmas.qty;
     }, 1000); //every second
 });
@@ -81,8 +93,34 @@ cookieApp.controller("factories", function($scope, $interval, FactCookieJar, Fac
         }
     };
 
-    var update = $interval(function() {
+    if($scope.factories.update)
+        $interval.cancel($scope.factories.update);
+
+    $scope.factories.update = $interval(function() {
         $scope.ovens.qty += $scope.factories.qty;
     }, 1000); //every second
+});
+
+cookieApp.controller("retirementHomes", function($scope, $interval, FactCookieJar, FactRetirementHomes, FactGrandmas) {
+    $scope.retirementHomes = FactRetirementHomes;
+    $scope.grandmas = FactGrandmas;
+    $scope.cookies = FactCookieJar;
+
+    $scope.buyRetirementHome = function() {
+        if($scope.cookies.qty >= $scope.retirementHomes.cost)
+        {
+            $scope.cookies.qty -= $scope.retirementHomes.cost;
+            $scope.retirementHomes.qty += 1;
+            $scope.retirementHomes.cost += 10000;
+        }
+    };
+
+    if($scope.retirementHomes.update)
+        $interval.cancel($scope.retirementHomes.update);
+
+    $scope.retirementHomes.update = $interval(function() {
+        $scope.grandmas.qty += $scope.retirementHomes.qty;
+    }, 1000);
+    
 });
 
